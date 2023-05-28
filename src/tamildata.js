e015978d92f5f55cd552583aef8bf73f83f2fe8e
Ahamedanas123 <ahamedanasali0@gmail.com>
2023-05-28 13:00:36 +0530

    done

---
+++ src/tamildata.js
@@ -0,0 +1,48 @@
+export const tamildata = [
+  { 
+    id:"100",
+    image: "https://m.media-amazon.com/images/I/A1FzdoPiSwL.jpg",
+    name: "Ponniyin Selvan",
+    author: "Kalki",
+    content:"Ponniyin Selvan is a Tamil historical fiction novel written by Kalki based on real events. It narrates the story of Arulmozhivarman (later crowned as Rajaraja Chola I), one of the kings of the Chola Dynasty during the 10th and 11th centuries. This historic novel serialised for a period of three and a half years.",
+  },
+  {
+    id:"101",
+    image: "https://routemybook.com/uploads/productImage/product1598101230.jpg",
+    name: "Tholkappiyam",
+    author: "Tholkappiyar",
+    content:"Tholkāppiyam, also romanised as Tholkaappiyam (Tamil: தொல்காப்பியம், lit., is the most ancient extant Tamil grammar text and the oldest extant long work of Tamil literature. The surviving manuscripts of the Tolkappiyam consists of three books (atikaram), each with nine chapters (iyal), with a cumulative total of 1,610 (483+463+664) sutras in the nūṛpā meter.[4][note 1] It is a comprehensive text on grammar, and includes sutras on orthography, phonology, etymology, morphology, semantics, prosody, sentence structure and the significance of context in language.",
+
+  },
+
+  {
+    id:"102",
+    image: "https://m.media-amazon.com/images/I/51d5qlCMp4L.jpg",
+    name: "Thirukkural",
+    author: "Thiruvalluvar",
+    content:"The Tirukkuṟaḷ (Tamil: திருக்குறள், lit.'sacred verses'), or shortly the Kural (Tamil: குறள்), is a classic Tamil language text consisting of 1,330 short couplets, or kurals, of seven words each.The text is divided into three books with aphoristic teachings on virtue (aram), wealth (porul) and love (inbam), respectively. Considered one of the greatest works ever written on ethics and morality, it is widely acknowledged for its universality and secular nature.Its authorship is traditionally attributed to Valluvar, also known in full as Thiruvalluvar. The text has been dated variously from 300 BCE to 5th century CE. The traditional accounts describe it as the last work of the third Sangam, but linguistic analysis suggests a later date of 450 to 500 CE and that it was composed after the Sangam period.",
+    
+
+  },
+
+  {
+    id:"103",
+    image: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/31RrrXOWRsL._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    name: "Silapathikaram",
+    author: "Ilango adigal",
+    content:"The Silappathikaram tells of the young merchant Kovalan's marriage to the virtuous Kannaki (Kannagi), his love for the courtesan Matavi, and his consequent ruin and exile in Maturai, where he is unjustly executed after trying to sell his wife's anklet to a wicked goldsmith who had stolen the queen's anklet and charged ...",
+   
+
+  },
+  {
+    id:"104",
+  image: "https://bookwomb.com/pub/media/catalog/product/s/e/seevagasindhamani_front_1.jpg",
+  name: "Civaka Cintamani",
+  author: "Tirutakkatevar",
+  content:"Jivaka, the Fabulous Gem'), also spelled as Jivaka Chintamani, is one of the five great Tamil epics. Authored by a Madurai-based Jain ascetic Tiruttakkatēvar in the early 10th century, the epic is a story of a prince who is the perfect master of all arts, perfect warrior and perfect lover with numerous wives.",
+ 
+
+},
+
+
+];
